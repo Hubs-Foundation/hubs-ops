b45140e67985acf9e758b7629ac3487b92b6b3d0
Greg Fodor <gfodor@gmail.com>
2017-10-07 23:25:51 -0700

    Remove output

--- terraform/modules/base/output.tf
+++ terraform/modules/base/output.tf
@@ -2,10 +2,6 @@ output "base_policy_arn" {
   value = "${aws_iam_policy.base-policy.arn}"
 }
 
-output "db_monitoring_policy_arn" {
-  value = "${aws_iam_policy.db-monitoring-policy.arn}"
-}
-
 output "mr_ssh_key_id" {
   value = "${aws_key_pair.mr-ssh-key.id}"
 }
