3f229f01bbab8e4856dd56a1236fd4c5b4deacab
Greg Fodor <gfodor@gmail.com>
2018-04-16 18:41:28 -0700

    Add HTTP listeners to support SSL redirection

--- terraform/modules/ret/main.tf
+++ terraform/modules/ret/main.tf
@@ -99,7 +99,7 @@ resource "aws_alb_target_group" "ret-alb-group-http" {
   }
 }
 
-resource "aws_alb_listener" "ret-alb-listener" {
+resource "aws_alb_listener" "ret-ssl-alb-listener" {
   load_balancer_arn = "${aws_alb.ret-alb.arn}"
   port = 443
 
@@ -108,6 +108,18 @@ resource "aws_alb_listener" "ret-alb-listener" {
 
   certificate_arn = "${data.aws_acm_certificate.ret-alb-listener-cert.arn}"
   
+  default_action {
+    target_group_arn = "${aws_alb_target_group.ret-alb-group-http.arn}"
+    type = "forward"
+  }
+}
+
+resource "aws_alb_listener" "ret-clear-alb-listener" {
+  load_balancer_arn = "${aws_alb.ret-alb.arn}"
+  port = 80
+
+  protocol = "HTTP"
+
   default_action {
     target_group_arn = "${aws_alb_target_group.ret-alb-group-http.arn}"
     type = "forward"
@@ -377,7 +389,7 @@ resource "aws_alb_target_group" "ret-smoke-alb-group-http" {
   }
 }
 
-resource "aws_alb_listener" "ret-smoke-alb-listener" {
+resource "aws_alb_listener" "ret-smoke-ssl-alb-listener" {
   load_balancer_arn = "${aws_alb.ret-smoke-alb.arn}"
   port = 443
 
@@ -385,6 +397,18 @@ resource "aws_alb_listener" "ret-smoke-alb-listener" {
   ssl_policy = "ELBSecurityPolicy-2015-05"
 
   certificate_arn = "${data.aws_acm_certificate.ret-alb-listener-cert.arn}"
+  
+  default_action {
+    target_group_arn = "${aws_alb_target_group.ret-smoke-alb-group-http.arn}"
+    type = "forward"
+  }
+}
+
+resource "aws_alb_listener" "ret-smoke-clear-alb-listener" {
+  load_balancer_arn = "${aws_alb.ret-smoke-alb.arn}"
+  port = 80
+
+  protocol = "HTTP"
   
   default_action {
     target_group_arn = "${aws_alb_target_group.ret-smoke-alb-group-http.arn}"
