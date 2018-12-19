302127a83676b36911ab5f35ef82e8b73ce680c5
Greg Fodor <gfodor@gmail.com>
2018-12-19 02:24:32 +0000

    Ensure ytdl runs in bg

--- plans/youtube-dl-api-server/scripts/smoke-test-and-remove.sh
+++ plans/youtube-dl-api-server/scripts/smoke-test-and-remove.sh
@@ -8,6 +8,6 @@ PACKAGE_IDENT=$1
 hab pkg install -b core/curl
 hab pkg install /src/results/*.hart
 cd /hab/pkgs/$PACKAGE_IDENT
-bin/youtube-dl-server --number-processes 1
+bin/youtube-dl-server --number-processes 1 &
 sleep 5
 curl -i http://localhost:9191/api/play?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ | head -n 1 | grep '302 FOUND' || rm -rf /src/results
