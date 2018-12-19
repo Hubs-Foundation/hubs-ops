922da6fe00ecdb86186b44fd27a596fa2b84ea0d
Greg Fodor <gfodor@gmail.com>
2018-12-19 02:09:33 +0000

    Add smoke test

---
+++ plans/youtube-dl-api-server/scripts/smoke-test-and-remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Smoke tests the API server, and if the smoke test fails, removes all build artifacts (yes, this is a bit extreme but was the easiest)
+# way to ensure we definitely block corrupted builds
+
+PACKAGE_IDENT=$1
+
+hab pkg install core/curl
+cd /hab/pkgs/mozillareality/$packageIdent
+bin/youtube-dl-server --number-processes 1
+sleep 5
+curl -i http://localhost:9191/api/play?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ | head -n 1 | grep '302 FOUND' || rm -rf /src/results
