5283345457e9d784c67ffe770c35bc0bc87b121e
Greg Fodor <gfodor@gmail.com>
2019-01-30 03:53:28 +0000

    Fix up postgrest plan

--- plans/postgrest/habitat/plan.sh
+++ plans/postgrest/habitat/plan.sh
@@ -10,18 +10,37 @@ pkg_deps=(
   core/openssl
   core/zlib
   core/libpq
+  core/gmp
   core/cacerts
 )
 
 pkg_build_deps=(
   core/curl
+  core/patchelf
 )
 
 do_build() {
   return 0
 }
 
+do_unpack() {
+  mkdir -p "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
+
+  curl -SLO "https://github.com/PostgREST/postgrest/releases/download/v${pkg_version}/postgrest-v${pkg_version}-ubuntu.tar.xz" && tar xfvJ postgrest-v${pkg_version}-ubuntu.tar.xz \
+    -C "${HAB_CACHE_SRC_PATH}/${pkg_dirname}" \
+    --no-same-owner
+}
+
+do_prepare() {
+  patchelf --set-rpath "$LD_RUN_PATH" "${HAB_CACHE_SRC_PATH}/${pkg_dirname}/postgrest"
+  return 0
+}
+
 do_install() {
-  curl -SLO "https://github.com/PostgREST/postgrest/releases/download/v${pkg_version}/postgrest-v${pkg_version}-ubuntu.tar.xz" && tar xfvJ postgrest-v${pkg_version}-ubuntu.tar.xz && mv postgrest ${pkg_prefix}/bin
+  mv "${HAB_CACHE_SRC_PATH}/${pkg_dirname}/postgrest" "${pkg_prefix}/bin"
+  return 0
+}
+
+do_strip() {
   return 0
 }
