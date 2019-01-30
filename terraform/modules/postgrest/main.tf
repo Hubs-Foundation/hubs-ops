bb7f1234394c1476171064a0e3ce06d8eea0d2a3
Greg Fodor <gfodor@gmail.com>
2019-01-30 02:02:28 +0000

    Add initial terraform plan for postgrest

---
+++ terraform/modules/postgrest/main.tf
@@ -0,0 +1,118 @@
+variable "shared" { type = "map" }
+terraform { backend "s3" {} }
+provider "aws" { region = "${var.shared["region"]}", version = "~> 1.15" }
+provider "aws" { alias = "east", region = "us-east-1", version = "~> 1.15" }
+data "aws_availability_zones" "all" {}
+
+data "terraform_remote_state" "vpc" { backend = "s3", config = { key = "vpc/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+data "terraform_remote_state" "base" { backend = "s3", config = { key = "base/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+data "terraform_remote_state" "bastion" { backend = "s3", config = { key = "bastion/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+data "terraform_remote_state" "hab" { backend = "s3", config = { key = "hab/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+data "terraform_remote_state" "ret-db" { backend = "s3", config = { key = "ret-db/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+data "terraform_remote_state" "ret" { backend = "s3", config = { key = "ret/terraform.tfstate", bucket = "${var.shared["state_bucket"]}", region = "${var.shared["region"]}", dynamodb_table = "${var.shared["dynamodb_table"]}", encrypt = "true" } }
+
+data "aws_ami" "hab-base-ami" {
+  most_recent = true
+  owners = ["self"]
+
+  filter {
+    name = "name"
+    values = ["hab-base-*"]
+  }
+}
+
+resource "aws_security_group" "postgrest" {
+  name = "${var.shared["env"]}-postgrest"
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+
+  egress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = "443"
+    to_port = "443"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # PostgREST HTTP
+  ingress {
+    from_port = "${var.postgrest_http_port}"
+    to_port = "${var.postgrest_http_port}"
+    protocol = "tcp"
+    security_groups = ["${data.terraform_remote_state.bastion.bastion_security_group_id}"]
+  }
+
+  # SSH
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    security_groups = ["${data.terraform_remote_state.bastion.bastion_security_group_id}"]
+  }
+
+  # NTP
+  egress {
+    from_port = "123"
+    to_port = "123"
+    protocol = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "postgrest" {
+  name = "${var.shared["env"]}-postgrest"
+  assume_role_policy = "${var.shared["ec2_role_policy"]}"
+}
+
+resource "aws_iam_role_policy_attachment" "bastion-base-policy" {
+  role = "${aws_iam_role.postgrest.name}"
+  policy_arn = "${data.terraform_remote_state.base.base_policy_arn}"
+}
+
+resource "aws_iam_instance_profile" "postgrest" {
+  name = "${var.shared["env"]}-postgrest"
+  role = "${aws_iam_role.postgrest.id}"
+}
+
+resource "aws_launch_configuration" "postgrest" {
+  image_id = "${data.aws_ami.hab-base-ami.id}"
+  instance_type = "${var.postgrest_instance_type}"
+  security_groups = [
+    "${aws_security_group.postgrest.id}",
+    "${data.terraform_remote_state.ret-db.ret_db_consumer_security_group_id}",
+    "${data.terraform_remote_state.hab.hab_ring_security_group_id}",
+  ]
+  key_name = "${data.terraform_remote_state.base.mr_ssh_key_id}"
+  iam_instance_profile = "${aws_iam_instance_profile.postgrest.id}"
+  associate_public_ip_address = false
+  lifecycle { create_before_destroy = true }
+  root_block_device { volume_size = 64 }
+  user_data = <<EOF
+#!/usr/bin/env bash
+while ! nc -z localhost 9632 ; do sleep 1; done
+systemctl restart systemd-sysctl.service
+
+sudo /usr/bin/hab svc load mozillareality/postgrest --strategy ${var.postgrest_restart_strategy} --url https://bldr.habitat.sh --channel ${var.postgrest_channel}
+sudo /usr/bin/hab svc load mozillareality/dd-agent --strategy at-once --url https://bldr.habitat.sh --channel stable
+EOF
+}
+
+resource "aws_autoscaling_group" "postgrest" {
+  name = "${var.shared["env"]}-postgrest"
+  launch_configuration = "${aws_launch_configuration.postgrest.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  vpc_zone_identifier = ["${data.terraform_remote_state.vpc.private_subnet_ids}"]
+
+  min_size = "1"
+  max_size = "1"
+
+  lifecycle { create_before_destroy = true }
+  tag { key = "env", value = "${var.shared["env"]}", propagate_at_launch = true }
+  tag { key = "host-type", value = "${var.shared["env"]}-postgrest", propagate_at_launch = true }
+  tag { key = "hab-ring", value = "${var.shared["env"]}", propagate_at_launch = true }
+}
