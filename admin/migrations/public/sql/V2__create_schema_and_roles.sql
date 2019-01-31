105133e494a390dd47658c9a746034d920ae99ca
Greg Fodor <gfodor@gmail.com>
2019-01-31 19:23:53 +0000

    Add view construction functions for ret_admin

--- admin/migrations/public/sql/V2__create_schema_and_roles.sql
+++ admin/migrations/public/sql/V2__create_schema_and_roles.sql
@@ -8,3 +8,5 @@ grant postgrest_anonymous to postgrest_authenticator;
 grant ret_admin to postgrest_authenticator;
 
 grant usage on schema ret0_admin to ret_admin;
+grant usage on schema ret0 to ret_admin;
+grant usage on ret0.table_id_seq to ret_admin;
--- admin/migrations/ret_admin/sql/V1__create_scene_views.sql
+++ admin/migrations/ret_admin/sql/V1__create_scene_views.sql
@@ -1,3 +1,37 @@
-create view scenes as (select * from ret0.scenes);
+create or replace function create_or_replace_admin_view(name text)
+returns void as
+$$
+declare
+pk character varying(255);
+begin
 
-grant all privileges on all tables in schema ret0_admin to ret_admin;
+-- Get the primary key
+SELECT
+  pg_attribute.attname into pk
+FROM pg_index, pg_class, pg_attribute, pg_namespace
+WHERE
+  pg_class.oid = ('ret0.' || name)::regclass AND
+  indrelid = pg_class.oid AND
+  nspname = 'ret0' AND
+  pg_class.relnamespace = pg_namespace.oid AND
+  pg_attribute.attrelid = pg_class.oid AND
+  pg_attribute.attnum = any(pg_index.indkey)
+ AND indisprimary;
+
+-- Create a view with the primary key renamed to id 
+execute 'create or replace view ' || name || ' as (select ' || pk || ' as id, '
+|| array_to_string(ARRAY(SELECT 'o' || '.' || c.column_name
+        FROM information_schema.columns As c
+            WHERE table_name = name AND table_schema = 'ret0'
+            AND  c.column_name NOT IN(pk)
+    ), ',') ||
+				' from ret0.' || name || ' as o)';
+end
+
+execute 'grant all privileges on all tables in schema ret0_admin to ret_admin'
+
+$$ language plpgsql;
+
+select create_or_replace_admin_view('scenes');
+select create_or_replace_admin_view('accounts');
+select create_or_replace_admin_view('owned_files');
