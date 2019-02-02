0f764e7a7794418483c18e7bb22e1464ed6c6f8d
Greg Fodor <gfodor@gmail.com>
2019-02-02 04:57:00 +0000

    try fixing up mistake with schema name

--- db/ret_admin/sql/V3__fix_up_permissions.sql
+++
@@ -1,9 +0,0 @@
-revoke all on ret_admin.scenes from ret_admin;
-revoke all on ret_admin.accounts from ret_admin;
-revoke all on ret_admin.owned_files from ret_admin;
-revoke all on ret_admin.hubs_metrics from ret_admin;
-
-grant select, insert, update on ret_admin.scenes to ret_admin;
-grant select, insert, update on ret_admin.accounts to ret_admin;
-grant select, insert, update on ret_admin.owned_files to ret_admin;
-grant select on ret_admin.hubs_metrics to ret_admin;
