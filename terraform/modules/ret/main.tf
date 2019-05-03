582e8f895ba82c9fdbcafc58b7909d0a794c8791
Greg Fodor <gfodor@gmail.com>
2019-05-03 03:37:52 +0000

    Move to port 587

--- terraform/modules/ret/main.tf
+++ terraform/modules/ret/main.tf
@@ -249,8 +249,8 @@ resource "aws_security_group" "ret" {
 
   # Outbound SMTP
   egress {
-    from_port = "25"
-    to_port = "25"
+    from_port = "587"
+    to_port = "587"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
