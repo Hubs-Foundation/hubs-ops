22b6091a08bf820a65fd0c2e796b9d829af1b98c
Greg Fodor <gfodor@gmail.com>
2019-10-24 05:44:45 +0000

    Add AMI injection script

---
+++ cloudformation/build-and-publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ -z "$HUBS_OPS_SECRETS_PATH" ]]; then
+  echo -e "You'll need to clone the ops secrets:
+
+git clone https://git-codecommit.us-west-1.amazonaws.com/v1/repos/hubs-ops-secrets
+
+Then set HUBS_OPS_SECRETS_PATH to point to the cloned repo."
+  exit 1
+fi
+
+cfn-flip stack.yaml - | ./inject-amis.js "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.json" | cfn-flip -y - stack_injected.yaml
+aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_injected.yaml s3://hubs-cloud/stack.yaml 
+rm stack.json
+rm stack_injected.yaml
