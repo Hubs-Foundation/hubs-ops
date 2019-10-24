ab05bf0ffa928b0406a6b07659d6b631503876f2
Greg Fodor <gfodor@gmail.com>
2019-10-24 06:03:40 +0000

    No more flie

--- cloudformation/publish.sh
+++ cloudformation/publish.sh
@@ -11,5 +11,4 @@ fi
 
 cfn-flip stack.yaml - | ./inject-amis.js "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.json" | cfn-flip -y - stack_injected.yaml
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_injected.yaml s3://hubs-cloud/stack.yaml 
-rm stack.json
 rm stack_injected.yaml
