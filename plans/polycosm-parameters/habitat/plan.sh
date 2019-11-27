54010c236bc468c56b0c9d61f4c4f95027fbf512
Greg Fodor <gfodor@gmail.com>
2019-11-27 03:01:31 +0000

    Add parameter service, add user key, remove service keys as separate resources

---
+++ plans/polycosm-parameters/habitat/plan.sh
@@ -0,0 +1,24 @@
+pkg_name=polycosm-parameters
+pkg_origin=mozillareality
+pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
+
+pkg_version="1.0.0"
+pkg_license=('MPLv2')
+pkg_description="Dummy service for polycosm configuration."
+pkg_upstream_url="https://hubs.mozilla.com/"
+pkg_build_deps=(
+    core/coreutils
+    core/bash
+)
+
+pkg_deps=(
+    core/coreutils
+)
+
+do_build() {
+  :
+}
+
+do_install() {
+  :
+}
