7584fbc30404f23ac1dd86731b963162b136e8c8
Greg Fodor <gfodor@gmail.com>
2019-12-05 06:23:46 +0000

    Use new manifest file

--- cloudformation/publish.sh
+++ cloudformation/publish.sh
@@ -9,7 +9,7 @@ Then set HUBS_OPS_SECRETS_PATH to point to the cloned repo."
   exit 1
 fi
 
-cfn-flip stack.yaml - | ./inject-amis.js "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.json" | cfn-flip -y - stack_injected.yaml
+cfn-flip stack.yaml - | ./inject-amis.js "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.aws.json" | cfn-flip -y - stack_injected.yaml
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_injected.yaml s3://hubs-cloud/stack.yaml 
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" "$HUBS_OPS_SECRETS_PATH/packer/polycosm/files/polycosm_start.aws.sh" s3://polycosm-assets-prod-77ae26402152f4ea/polycosm_start.aws.sh
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" "$HUBS_OPS_SECRETS_PATH/packer/polycosm/files/polycosm_start.single.sh" s3://polycosm-assets-prod-77ae26402152f4ea/polycosm_start.single.sh
