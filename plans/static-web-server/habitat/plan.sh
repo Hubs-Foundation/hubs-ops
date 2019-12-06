b0c9ef795a20765a433abdd5d1c4770f02c287bc
Greg Fodor <gfodor@gmail.com>
2019-12-06 02:46:35 +0000

    Add plans for single hosts

---
+++ plans/static-web-server/habitat/plan.sh
@@ -0,0 +1,15 @@
+pkg_name=static-web-server
+pkg_version=1.0
+pkg_origin=mozillareality
+pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
+pkg_license=('unlicense')
+pkg_description="Really simple static web server"
+pkg_deps=(core/python)
+
+do_build() {
+  :
+}
+
+do_install() {
+  :
+}
