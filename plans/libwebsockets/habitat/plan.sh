999bf6e87259781a676da690486215d58260c216
Greg Fodor <gfodor@gmail.com>
2020-04-11 00:43:48 +0000

    OpenSSL update for websockets

--- plans/libwebsockets/habitat/plan.sh
+++ plans/libwebsockets/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_version="2.4.2"
 pkg_source="https://github.com/warmcat/libwebsockets/archive/v2.4.2.tar.gz"
 pkg_shasum="73012d7fcf428dedccc816e83a63a01462e27819d5537b8e0d0c7264bfacfad6"
 pkg_license=('LGPL-2.1')
-pkg_build_deps=(core/make core/gcc core/cmake core/openssl core/git)
+pkg_build_deps=(core/make core/gcc core/cmake mozillareality/openssl core/git)
 pkg_deps=(mozillareality/zlib)
 pkg_lib_dirs=(lib)
 pkg_bin_dirs=(bin)
@@ -26,7 +26,7 @@ do_build() {
     -DLWS_MAX_SMP=1 \
     -DLWS_WITHOUT_TESTAPPS=ON \
     -DCMAKE_C_FLAGS="-fpic" \
-    -DOPENSSL_ROOT_DIR=$(pkg_path_for core/openssl) ..
+    -DOPENSSL_ROOT_DIR=$(pkg_path_for mozillareality/openssl) ..
 
   make
 }
