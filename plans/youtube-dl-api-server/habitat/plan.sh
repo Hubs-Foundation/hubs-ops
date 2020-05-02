d02baaf9e63b995e59990c728b7ab34ee4adfd7b
Kevin Lee <kevin@infinite-lee.com>
2020-05-01 18:29:48 -0700

    set versions for the existing deps, not for aws-cli

--- plans/youtube-dl-api-server/habitat/plan.sh
+++ plans/youtube-dl-api-server/habitat/plan.sh
@@ -8,11 +8,10 @@ pkg_upstream_url="https://github.com/mozillareality/youtube-dl-api-server"
 pkg_source="https://github.com/mozillareality/youtube-dl-api-server/archive/${pkg_version}.tar.gz"
 pkg_shasum="6107513539ac18ef14377a0ecea55e54248e0113d7bef479da98a3dc19dad8d1"
 pkg_deps=(
-  core/envdir
-  core/lzop 
-  core/pv 
-  core/python 
-  core/aws-cli/1.16.118/20190305224525 # AWS cli used for run hook when uploading to S3)
+  core/envdir/1.0.1/20200404003559
+  core/lzop/1.04/20200404063539
+  core/pv1.6.0/20200403140749
+  core/python/3.7.0/20200310040007
 pkg_bin_dirs=(bin)
 
 do_prepare() {
