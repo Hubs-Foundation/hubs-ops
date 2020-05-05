b82bd24270defabb2c931261d1240847329aa6ed
Dominick D'Aniello <netpro2k@gmail.com>
2020-05-05 15:24:12 -0700

    Fix pinned dep versions for youtube-dl-api-server

--- plans/youtube-dl-api-server/habitat/plan.sh
+++ plans/youtube-dl-api-server/habitat/plan.sh
@@ -8,10 +8,10 @@ pkg_upstream_url="https://github.com/mozillareality/youtube-dl-api-server"
 pkg_source="https://github.com/mozillareality/youtube-dl-api-server/archive/${pkg_version}.tar.gz"
 pkg_shasum="6107513539ac18ef14377a0ecea55e54248e0113d7bef479da98a3dc19dad8d1"
 pkg_deps=(
-  core/envdir/1.0.1/20200404003559
-  core/lzop/1.04/20200404063539
-  core/pv/1.6.0/20200403140749
-  core/python/3.7.0/20200310040007
+  core/envdir/1.0.1/20190305224317
+  core/lzop/1.04/20190116190143
+  core/pv/1.6.0/20190116190154
+  core/python/3.7.0/20190305212847 
 )
 pkg_bin_dirs=(bin)
 
