0b2b33b37a5e4516ec3d7f7f7534e608c0801196
Robin K Wilson <robin.kath.wilson@gmail.com>
2021-11-22 14:00:02 -0800

    removed replace in cloudformation/publish.sh script

--- cloudformation/publish.sh
+++ cloudformation/publish.sh
@@ -18,7 +18,7 @@ aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_
 cfn-flip stack.yaml - | ./prep-stack.js personal "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.aws.json" | cfn-flip -y - stack_injected.yaml
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_injected.yaml s3://hubs-cloud/stack-personal.yaml 
 
-cfn-flip stack.yaml - | ./prep-stack.js beta "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.aws-beta.json" | sed 's/applications\/keymaster/applications\/keymaster-public/g' | cfn-flip -y - stack_injected.yaml
+cfn-flip stack.yaml - | ./prep-stack.js beta "$HUBS_OPS_SECRETS_PATH/packer/polycosm/manifest.aws-beta.json" | cfn-flip -y - stack_injected.yaml
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" stack_injected.yaml s3://hubs-cloud/stack-beta.yaml 
 
 aws s3 cp --region us-west-1 --acl public-read --cache-control "no-cache" "$HUBS_OPS_SECRETS_PATH/packer/polycosm/files/polycosm_start.single.sh" s3://polycosm-assets-prod-77ae26402152f4ea/polycosm_start.single.sh
