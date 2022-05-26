abe1c7fd24ab56784b3744679e34fecbc6a11452
Brandon Patterson <lilamar31@gmail.com>
2022-05-26 14:42:37 -0400

    Delete compare.sh

--- .github/workflows/compare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e 
-
-if [ "$(git rev-parse qa-test)" != "$(git rev-parse master)" ];  then 
-    echo "They're Different. Moving Branch pointer" 
-    git update-ref -m "Preping Qa-test"
-fi  
