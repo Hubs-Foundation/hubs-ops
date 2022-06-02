45eb1a4b62a9f02dbbf4a4bbe26ff308074579c9
Brian Peiris <brianpeiris@gmail.com>
2022-06-02 18:56:41 -0400

    prettier

--- workers/cors-proxy-internal.js
+++ workers/cors-proxy-internal.js
@@ -1,51 +1,75 @@
 const ALLOWED_ORIGINS = [
-  "https://hubs.local:8080", "https://hubs.local:9090", "https://hubs.local:4000", "https://dev.reticulum.io", "https://smoke-dev.reticulum.io",
-  "https://hubs.mozilla.com", "https://smoke-hubs.mozilla.com", "https://photomnemonic-utils.reticulum.io"
+  "https://hubs.local:8080",
+  "https://hubs.local:9090",
+  "https://hubs.local:4000",
+  "https://dev.reticulum.io",
+  "https://smoke-dev.reticulum.io",
+  "https://hubs.mozilla.com",
+  "https://smoke-hubs.mozilla.com",
+  "https://photomnemonic-utils.reticulum.io",
 ];
 const PROXY_HOST = "https://hubs-proxy.com";
 
-addEventListener("fetch", e => {
+addEventListener("fetch", (e) => {
   const request = e.request;
   const origin = request.headers.get("Origin");
   const proxyUrl = new URL(PROXY_HOST);
   let targetUrl = request.url.substring(PROXY_HOST.length + 1).replace(/^http(s?):\/([^/])/, "http$1://$2");
-  
+
   if (!targetUrl.startsWith("http://") && !targetUrl.startsWith("https://")) {
     targetUrl = proxyUrl.protocol + "//" + targetUrl;
   }
-  
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.delete("Origin"); // Some domains disallow access from improper Origins
 
-  e.respondWith((async () => {
-    const res = await fetch(targetUrl, { headers: requestHeaders, method: request.method, redirect: "manual", referrer: request.referrer, referrerPolicy: request.referrerPolicy });
-    const responseHeaders = new Headers(res.headers);
+  e.respondWith(
+    (async () => {
+      const res = await fetch(targetUrl, {
+        headers: requestHeaders,
+        method: request.method,
+        redirect: "manual",
+        referrer: request.referrer,
+        referrerPolicy: request.referrerPolicy,
+      });
+      const responseHeaders = new Headers(res.headers);
 
-    const redirectLocation = responseHeaders.get("Location") || responseHeaders.get("location");
+      const redirectLocation = responseHeaders.get("Location") || responseHeaders.get("location");
 
-    if(redirectLocation) {
-      if (!redirectLocation.startsWith("/")) {
-        responseHeaders.set("Location",  proxyUrl.protocol + "//" + proxyUrl.host + "/" + redirectLocation);
-      } else {
-        const tUrl = new URL(targetUrl);
-        responseHeaders.set("Location",  proxyUrl.protocol + "//" + proxyUrl.host + "/" + tUrl.origin + redirectLocation);
+      if (redirectLocation) {
+        if (!redirectLocation.startsWith("/")) {
+          responseHeaders.set("Location", proxyUrl.protocol + "//" + proxyUrl.host + "/" + redirectLocation);
+        } else {
+          const tUrl = new URL(targetUrl);
+          responseHeaders.set(
+            "Location",
+            proxyUrl.protocol + "//" + proxyUrl.host + "/" + tUrl.origin + redirectLocation
+          );
+        }
       }
-    }
 
-    if (origin && ALLOWED_ORIGINS.indexOf(origin) >= 0) {
-      responseHeaders.set("Access-Control-Allow-Origin", origin);
-      responseHeaders.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
-      responseHeaders.set("Access-Control-Allow-Headers", "Range");
-      responseHeaders.set("Access-Control-Expose-Headers", "Accept-Ranges, Content-Encoding, Content-Length, Content-Range");
-    }
+      if (origin && ALLOWED_ORIGINS.indexOf(origin) >= 0) {
+        responseHeaders.set("Access-Control-Allow-Origin", origin);
+        responseHeaders.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
+        responseHeaders.set("Access-Control-Allow-Headers", "Range");
+        responseHeaders.set(
+          "Access-Control-Expose-Headers",
+          "Accept-Ranges, Content-Encoding, Content-Length, Content-Range"
+        );
+      }
 
-    responseHeaders.set("Vary", "Origin");
-    responseHeaders.set('X-Content-Type-Options', "nosniff");
-    
-    const responseContentType = responseHeaders.get("Content-Type").toLowerCase();
-    if (responseContentType.includes("script")) responseHeaders.set("Content-Type", "text/plain");
-    if (responseContentType.includes("html")) responseHeaders.set("Content-Type", "text/plain");
+      responseHeaders.set("Vary", "Origin");
+      responseHeaders.set("X-Content-Type-Options", "nosniff");
 
-    return new Response(res.body, { status: res.status, statusText: res.statusText, headers: responseHeaders });
-  })());
+      const responseContentType = responseHeaders.get("Content-Type").toLowerCase();
+      if (responseContentType.includes("script")) responseHeaders.set("Content-Type", "text/plain");
+      if (responseContentType.includes("html")) responseHeaders.set("Content-Type", "text/plain");
+
+      return new Response(res.body, {
+        status: res.status,
+        statusText: res.statusText,
+        headers: responseHeaders,
+      });
+    })()
+  );
 });
