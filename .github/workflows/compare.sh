357c20dc3fcbc1b02f34f2a46008d3a5a52b93ac
Brandon Patterson <lilmamar31@gmail.com>
2022-08-01 12:34:08 -0400

    added release code to job:launch

--- .github/workflows/compare.sh
+++
@@ -1,8 +0,0 @@
-main = ${{inputs.main}}
-releasing = ${{inputs.releasing}}
-
-if [ "$(git rev-parse $releasing)" != "$(git rev-parse $main)" ];  then 
-    echo  "::set-output name=diff::different"
-    else 
-     exit
-    fi
