144827263a5c3624afd65f9d837222d12da17aa5
Geng Tan <tanfarming@hotmail.com>
2023-03-02 12:44:24 -0500

    word

--- plans/youtube-dl-api-server/habitat/plan.sh
+++ plans/youtube-dl-api-server/habitat/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=youtube-dl-api-server
-pkg_version=0.4
+pkg_version=0.5
 pkg_origin=mozillareality
 pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
 pkg_license=('unlicense')
 pkg_description="A youtube-dl REST API server"
 pkg_upstream_url="https://github.com/mozillareality/youtube-dl-api-server"
-pkg_source="https://github.com/mozillareality/youtube-dl-api-server/archive/${pkg_version}.tar.gz"
-pkg_shasum="6107513539ac18ef14377a0ecea55e54248e0113d7bef479da98a3dc19dad8d1"
+# pkg_source="https://github.com/mozillareality/youtube-dl-api-server/archive/${pkg_version}.tar.gz"
+# pkg_shasum="6107513539ac18ef14377a0ecea55e54248e0113d7bef479da98a3dc19dad8d1"
 pkg_deps=(
   core/envdir/1.0.1/20190305224317
   core/lzop/1.04/20190116190143
@@ -21,14 +21,20 @@ do_prepare() {
 }
 
 do_build() {
-  python setup.py sdist
+  # python setup.py sdist
+  echo "ffffffffffffffff"
 }
 
 do_install() {
-  pip install "dist/youtube_dl_server-${pkg_version}.tar.gz"
+  # pip install "dist/youtube_dl_server-${pkg_version}.tar.gz"
+  cp -R ./ytdl-api/*.py ${pkg_prefix}
+  cp -R ./ytdl-api-deps/* ${pkg_prefix}/
   pip install gunicorn
   rm -rf dist build
 
   # Write out versions of all pip packages to package
   pip freeze > "$pkg_prefix/requirements.txt"
+
+  ls -lha ${pkg_prefix}
+
 }
