ede88718ae5b07df660a9ba2d18feaffe97fe27c
Geng Tan <tanfarming@hotmail.com>
2023-03-03 00:45:03 -0500

    cleanup

--- plans/youtube-dl-api-server/habitat/plan.sh
+++ plans/youtube-dl-api-server/habitat/plan.sh
@@ -5,8 +5,6 @@ pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
 pkg_license=('unlicense')
 pkg_description="A youtube-dl REST API server"
 pkg_upstream_url="https://github.com/mozillareality/youtube-dl-api-server"
-# pkg_source="https://github.com/mozillareality/youtube-dl-api-server/archive/${pkg_version}.tar.gz"
-# pkg_shasum="6107513539ac18ef14377a0ecea55e54248e0113d7bef479da98a3dc19dad8d1"
 pkg_deps=(
   core/envdir/1.0.1/20190305224317
   core/lzop/1.04/20190116190143
@@ -26,7 +24,6 @@ do_build() {
 }
 
 do_install() {
-  # pip install "dist/youtube_dl_server-${pkg_version}.tar.gz"
   cp -R ./ytdl-api/*.py ${pkg_prefix}
   cp -R ./ytdl-api-deps/* ${pkg_prefix}/
   pip install gunicorn
